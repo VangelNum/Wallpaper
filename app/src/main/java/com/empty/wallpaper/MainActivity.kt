cf46e2f14f398bb27e307b792bdabeae4df4a277
VangelNum <vangelnum@gmail.com>
2022-08-03 18:08:20 +0400

    Initial commit

---
+++ app/src/main/java/com/empty/wallpaper/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.empty.wallpaper
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.empty.wallpaper.ui.theme.WallpaperTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ListItem("","")
+
+        }
+    }
+}
+
+
+@Composable
+private fun ListItem(name: String, prof: String) {
+   Card(modifier = Modifier
+       .fillMaxWidth()
+       .padding(10.dp),
+   shape = RoundedCornerShape(15.dp),
+   elevation = 5.dp)
+   {
+        Box() {
+           Row(verticalAlignment = Alignment.CenterVertically) {
+               Image(painter = painterResource(id = R.drawable.ghoul1),
+                   contentDescription = "Image",
+                   contentScale = ContentScale.Crop,
+                   modifier = Modifier
+                       .padding(5.dp)
+                       .size(64.dp)
+                       .clip(CircleShape))
+               Column() {
+                   Text(text = "check")
+                   Text(text = "check")
+                   Text(text = "check")
+               }
+
+           }
+        }
+   }
+}
